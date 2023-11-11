895c711102edac3eaccc98033b36e03b3be4ac35
Daniel Reszka <dannn.r@gmail.com>
2023-11-11 18:22:50 +0100

    cycleImageDataHue: set custom sat + lum

--- src/lib/utils/canvas.ts
+++ src/lib/utils/canvas.ts
@@ -1,9 +1,17 @@
 import { hslToRgb, rgbToHsl } from './colors'
 
-function cycleImageDataHue(data: Uint8ClampedArray, hueShift: number) {
+function cycleImageDataHue(
+  data: Uint8ClampedArray,
+  hueShift: number,
+  saturation = 60,
+  lightness = 50
+) {
   for (let i = 0; i < data.length; i += 4) {
     const [r, g, b] = [data[i], data[i + 1], data[i + 2]]
+
     const hsl = rgbToHsl(r, g, b)
+    hsl[1] = saturation
+    hsl[2] = lightness
 
     // Shift the hue value
     hsl[0] += hueShift
@@ -22,11 +30,9 @@ export function cycleCanvasHue(canvas: HTMLCanvasElement, hueShift = 3) {
   if (!canvas) return
   const ctx = canvas.getContext('2d')
   if (!ctx) return
+
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
-
-  const data = imageData.data
-  cycleImageDataHue(data, hueShift)
-
+  cycleImageDataHue(imageData.data, hueShift)
   ctx.putImageData(imageData, 0, 0)
 }
 
