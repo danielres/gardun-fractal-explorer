835912bec08517ed80aa7cdc464a3e5bf94a7264
Daniel Reszka <dannn.r@gmail.com>
2023-11-11 18:25:03 +0100

    drawMandelbrot(): get width & height from canvas element

--- src/lib/utils/mandelbrot.ts
+++ src/lib/utils/mandelbrot.ts
@@ -1,15 +1,17 @@
 import { paintRect } from './canvas'
 import { rgbTupleToStr } from './colors'
 
+export const colorModes = ['original', 'vivid', 'bw'] as const
+
 export function computeRgb(
   t: number,
   luminance: number,
-  mode: 'original' | 'vivid' | 'bw' = 'original'
+  mode: typeof colorModes[number]
 ): [number, number, number] {
   if (mode === 'vivid') {
-    const r = t * luminance * (Math.sin((t - 85) / 20) + 1)
-    const g = t * luminance * (Math.sin((t - 170) / 20) + 1)
-    const b = t * luminance * (Math.sin((t - 255) / 20) + 1)
+    const r = t * luminance * (Math.sin((t - 85) / 40) + 1)
+    const g = t * luminance * (Math.sin((t - 170) / 40) + 0.25)
+    const b = ((t * luminance) / 2) * (Math.sin((t - 255) / 40) + 1)
 
     return [r, g, b]
   }
@@ -21,27 +23,33 @@ export function computeRgb(
   return [t * 3, t, t * 0.5]
 }
 
-export function drawMandelbrot(
-  canvasEl: HTMLCanvasElement,
-  SIZE: number,
-  zoom: number,
-  translateX: number,
-  translateY: number,
-  iterations: number,
-  luminance: number,
+type Options = {
+  zoom: number
+  translateX: number
+  translateY: number
+  iterations: number
+  luminance: number
   blocksize: number
-) {
-  if (!canvasEl) return
-  const context = canvasEl.getContext('2d')
+}
 
+export function drawMandelbrot(canvasEl: HTMLCanvasElement, options: Options) {
+  if (!canvasEl) return
+
+  const width = canvasEl.width
+  const height = canvasEl.height
+  const aspectRatio = width / height
+
+  const context = canvasEl.getContext('2d')
   if (!context) return
 
-  context.clearRect(0, 0, SIZE, SIZE)
+  context.clearRect(0, 0, width, height)
 
-  for (let y = 1; y < SIZE; y++) {
-    for (let x = 1; x < SIZE; x++) {
-      const dx = (x - SIZE / 2) / (SIZE * zoom) - translateX
-      const dy = (y - SIZE / 2) / (SIZE * zoom) - translateY
+  const { zoom, translateX, translateY, iterations, luminance, blocksize } = options
+
+  for (let y = 1; y < height; y++) {
+    for (let x = 1; x < width; x++) {
+      const dx = (aspectRatio * (x - width / 2)) / (width * zoom) - translateX
+      const dy = (y - height / 2) / (height * zoom) - translateY
       let a = dx
       let b = dy
 
