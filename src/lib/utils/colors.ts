cc6a620b3568b0d337880feddd01e94055251564
Daniel Reszka <dannn.r@gmail.com>
2023-11-11 18:18:50 +0100

    utils/colors: fix rounding errors, cleanup

--- src/lib/utils/colors.ts
+++ src/lib/utils/colors.ts
@@ -1,3 +1,7 @@
+export function rgbTupleToStr([r, g, b]: [number, number, number]) {
+  return 'rgb(' + r + ',' + g + ',' + b + ')'
+}
+
 export function rgbToHsl(r: number, g: number, b: number) {
   r /= 255
   g /= 255
@@ -12,23 +16,16 @@ export function rgbToHsl(r: number, g: number, b: number) {
   if (max !== min) {
     const d = max - min
     s = l > 0.5 ? d / (2 - max - min) : d / (max + min)
-    switch (max) {
-      case r:
-        h = (g - b) / d + (g < b ? 6 : 0)
-        break
-      case g:
-        h = (b - r) / d + 2
-        break
-      case b:
-        h = (r - g) / d + 4
-        break
-    }
-    h /= 6
+    h =
+      max === r
+        ? ((g - b) / d + (g < b ? 6 : 0)) / 6
+        : max === g
+        ? ((b - r) / d + 2) / 6
+        : ((r - g) / d + 4) / 6
   }
 
-  return [h * 360, s * 100, l * 100]
+  return [Math.round(h * 360), Math.round(s * 100), Math.round(l * 100)]
 }
-
 export function hslToRgb(h: number, s: number, l: number) {
   h /= 360
   s /= 100
@@ -38,25 +35,22 @@ export function hslToRgb(h: number, s: number, l: number) {
   if (s === 0) {
     r = g = b = l // achromatic
   } else {
-    const hue2rgb = (p: number, q: number, t: number) => {
-      if (t < 0) t += 1
-      if (t > 1) t -= 1
-      if (t < 1 / 6) return p + (q - p) * 6 * t
-      if (t < 1 / 2) return q
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
-      return p
-    }
-
     const q = l < 0.5 ? l * (1 + s) : l + s - l * s
     const p = 2 * l - q
+
     r = hue2rgb(p, q, h + 1 / 3)
     g = hue2rgb(p, q, h)
     b = hue2rgb(p, q, h - 1 / 3)
   }
 
-  return [r * 255, g * 255, b * 255]
+  return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
 }
 
-export function rgbTupleToStr([r, g, b]: [number, number, number]) {
-  return 'rgb(' + r + ',' + g + ',' + b + ')'
+function hue2rgb(p: number, q: number, t: number) {
+  if (t < 0) t += 1
+  if (t > 1) t -= 1
+  if (t < 1 / 6) return p + (q - p) * 6 * t
+  if (t < 1 / 2) return q
+  if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+  return p
 }
