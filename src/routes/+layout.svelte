e360ef82e9358a5832d0d9dd50979dd139a0546a
Daniel Reszka <dannn.r@gmail.com>
2023-11-11 18:28:56 +0100

    layout: remove or disable all extras

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,33 +1,11 @@
 <script lang="ts">
   import '../app.postcss'
-  import { AppShell, AppBar } from '@skeletonlabs/skeleton'
+  // import { AppShell, AppBar } from '@skeletonlabs/skeleton'
 
-  // Floating UI for Popups
-  import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom'
-  import { storePopup } from '@skeletonlabs/skeleton'
-  storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow })
+  // // Floating UI for Popups
+  // import { computePosition, autoUpdate, flip, shift, offset, arrow } from '@floating-ui/dom'
+  // import { storePopup } from '@skeletonlabs/skeleton'
+  // storePopup.set({ computePosition, autoUpdate, flip, shift, offset, arrow })
 </script>
 
-<!-- App Shell -->
-<AppShell>
-  <svelte:fragment slot="header">
-    <!-- App Bar -->
-    <AppBar>
-      <svelte:fragment slot="lead">
-        <strong class="text-xl uppercase">Gardun fractal explorer</strong>
-      </svelte:fragment>
-      <svelte:fragment slot="trail">
-        <a
-          class="btn btn-sm variant-ghost-surface"
-          href="https://github.com/danielres/gardun-fractal-explorer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </svelte:fragment>
-    </AppBar>
-  </svelte:fragment>
-  <!-- Page Route Content -->
-  <slot />
-</AppShell>
+<slot />
